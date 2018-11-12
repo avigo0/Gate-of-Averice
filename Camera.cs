acd91899a320e926af246c5d3e98ee2b0a2054a2
avigo0 <44301676+avigo0@users.noreply.github.com>
2018-11-12 13:28:35 -0800

    Add files via upload
    This is a dummy camera script! Do not add this to the main repositories

---
+++ Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject player;
+    public float posX;
+    public float posY;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    void LateUpdate()
+    {
+        posX = player.transform.position.x;
+        posY = player.transform.position.y + 3;
+        Debug.Log(posX);
+        gameObject.transform.position = new Vector3(posX, posY, gameObject.transform.position.z);
+    }
+
+
+}
